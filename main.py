1f78a8999282898bbb9bc318a06ad41fee4c9f5a
AlexDev404 <AlexDev404@users.noreply.github.com>
2025-03-27 14:08:45 -0600

    feat: Add values for front, right, left

--- main.py
+++ main.py
@@ -20,33 +20,15 @@ counter = 0
 
 def set_color(value):  # Set color of line follower
     global lineFollower_color
-    if int(value) != 0:
+    if int(value) == 3:
         lineFollower_color = 'white'
+    elif int(value) == 2:
+        lineFollower_color = 'right'
+    elif int(value) == 1:
+        lineFollower_color = 'left'
     else:
         lineFollower_color = 'black'
     print("Updated color:", lineFollower_color)
-
-
-def head_to_island():  # Head to island function
-    while True:
-        if lineFollower_color == 'white':
-            control.stop()
-            sleep(10)
-            leave_island_setup()
-            # turn_180_degrees()
-            control.stop()
-            sleep(0.5)
-            continue
-        else:
-            distance = ultrasonicSensor.get_distance(port=7)
-            if distance > 12:
-                control.controlled_turn(30, 11.5)
-            elif distance < 5:
-                get_further_from_right_wall()
-            elif distance > 7:
-                get_closer_to_right_wall()
-            else:
-                control.forward_non_stop(15)
 
 
 def get_further_from_right_wall():
@@ -56,35 +38,33 @@ def get_further_from_right_wall():
 
 def get_closer_to_right_wall():
     # Swerve to the right
-    control.controlled_turn(30, int(13 * -1))
+    control.controlled_turn(22, int(13 * -1))
+    sleep(1.25)
+    control.forward_non_stop(30)
 
 
-def leave_island_setup():
-    global counter
-    counter = 0
-    control.stop()
-    control.move_backward(15, 300)
-
-
-def turn_180_degrees():
-    control.stop()
-    sleep(0.3)
-    # make a 180-degree turn
-    control.controlled_turn(5, 28)
-    sleep(1.3)
-    # stop
-    control.stop()
-
-
-def avoid_wall():
+def avoid_wall(distance):
     global counter
     control.stop()  # Stop when front wall is hit or too close
     board.set_color(5, 255, 0, 0)  # Set all LED to red
-    control.move_backward(50, 700)  # Move back a bit
-    # Turn left 90 degrees, might need some tweaking
-    # control.move_left(50, 700)
+    sleep(1)
+    # We will only do a left turn if we are on the white line
+    if (lineFollower_color == 'left') or (lineFollower_color == 'white' and distance <= 10):
+        control.move_backward(50, 750)  # Move back a bit
+        # A white line is classified as a wall to our right and a wall to our front
+        control.move_left(28, 1450)
+    elif lineFollower_color == 'right':  # If we are on black line, we will do a right turn
+        control.move_backward(50, 750)  # Move back a bit
+        # A black line is classified as a wall to our front and no wall to our right
+        control.move_right(28, 1450)
+    elif lineFollower_color == 'white':
+        control.move_backward(50, 700)
+    # Commit to the turn
+    control.move_forward(28, 1650)
+    # else:
+    #     control.forward_non_stop(15)
     control.stop()  # Stop moving
-    sleep(1)  # Sleep for 1 second for line follower to update
+    sleep(0.5)  # Sleep for 0.5 second for line follower to update
     # counter += 1
 
 
@@ -110,27 +90,31 @@ def main():
         print(float(roll) < -30.0, float(pitch) < -1.0)
         # Continuously read the line follower data and update color
         lineFollower.read(set_color)
-        if lineFollower_color == 'white':
-            avoid_wall()
+        control.stop()
+        if lineFollower_color == 'white' or lineFollower_color == 'left' or lineFollower_color == 'right':
+            avoid_wall(distance)
             # Check if we are in island for a 180-degree turn
             # if counter == 4:
             #     turn_180_degrees()
                 # Head to island
                 # head_to_island()
+        # elif lineFollower_color == 'black' and distance >= 5:  # If we are on black line, we will do a right turn
+        #     # A black line is classified as a wall to our right and no wall to our front
+        #     control.move_right(50, 700)
         # Yank the robot out of the wall if it crashes
         elif lineFollower_color == 'black' and float(roll) < -30.0:
             control.move_backward(int(50 * unjam_retries), 500)
             unjam_retries += 1
-        elif distance > 12 or get_away_please is True:  # Is far from the wall
+        elif distance >= 5 or get_away_please is True:  # Is far from the wall
             get_closer_to_right_wall()
-            sleep(0.2)
+            sleep(0.5)
             get_away_please = False
-        elif distance < 2:  # Is too close to the wall
+        elif distance <= 2:  # Is too close to the wall
             get_further_from_right_wall()
             get_away_please = True
             sleep(3)
         else:
-            control.forward_non_stop(15)
+            control.forward_non_stop(20)
         # We're on level ground, reset the unjam retries
         if unjam_retries >= 2 and float(roll) > 0:
             # "Do not worry son, I am here"
