39e7f6631137d5fbb58016fb9cb29dec51780845
Abner Mencia <2020151792@ub.edu.bz>
2025-04-23 17:24:38 -0600

    update

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ def set_color(value): #set color of line follower
     else:
         lineFollower_color = 'black'
 
+# def exit_island():
+#
 
 def head_to_island(): #head to island function
 
@@ -49,47 +51,69 @@ def head_to_island(): #head to island function
     global control
     global counter
     global inside_Island
+    distance = ultrasonicSensor.get_distance(port=7)
 
     # Initial stop
     control.stop()
     sleep(1)
     print("\033[92m INSIDE ISLAND...\033[0m")  # Print in green
 
-    while True and inside_Island:
-        # Continuously read the line follower data and update color
+    if inside_Island:
+    #     # Continuously read the line follower data and update color
         lineFollower.read(set_color)
-        # Read ultrasonic distance
-        distance = ultrasonicSensor.get_distance(port=7)
-        # If the sensor reads 400, it's typically 'no object' or out of range.
-        # Instead of forcing it to zero, we can keep it at 400 or set a default:
-
-        print("\r" + " " * 50, end=" ")  # Clears the line
-        # Add color to the distance and line color, and different colors to their values
-        print(f"\r\033[94mDistance:\033[0m \033[92m{distance}\033[0m "
-              f" | \033[94mLine color:\033[0m \033[92m{lineFollower_color}\033[0m "
-              f"| \033[94mCounter :\033[0m \033[92m{counter}\033[0m "
-              f" | \033[94mInside Island:\033[0m \033[92m{inside_Island}\033[0m ", end=" ", flush=True)
+    #     # Read ultrasonic distance
+    #     distance = ultrasonicSensor.get_distance(port=7)
+    #     # If the sensor reads 400, it's typically 'no object' or out of range.
+    #     # Instead of forcing it to zero, we can keep it at 400 or set a default:
+    #
+    #     print("\r" + " " * 50, end=" ")  # Clears the line
+    #     # Add color to the distance and line color, and different colors to their values
+    #     print(f"\r\033[94mDistance:\033[0m \033[92m{distance}\033[0m "
+    #           f" | \033[94mLine color:\033[0m \033[92m{lineFollower_color}\033[0m "
+    #           f"| \033[94mCounter :\033[0m \033[92m{counter}\033[0m "
+    #           f" | \033[94mInside Island:\033[0m \033[92m{inside_Island}\033[0m ", end=" ", flush=True)
+        while lineFollower_color != 'white':
+            right_hand_rule()
 
         if lineFollower_color == 'white':
-            print("Stop 10 seconds.", end=" ", flush=True)
+            print("entering if in island", end=" ", flush=False)
+            distance = ultrasonicSensor.get_distance(port=7)
+            lineFollower.read(set_color)
+            print("Stop 5 seconds.", end=" ", flush=False)
             control.stop()
-            sleep(10)
-            leave_island_setup()
-        if distance > 11: #is far from the wall
-            print("Moving closer to wall.", end=" ", flush=True)
-            get_closer_to_wall()
+            sleep(5)
+            right_turn()
+          #if lineFollower_color == 'white':
+            lineFollower.read(set_color)
+            distance = ultrasonicSensor.get_distance(port=7)
+            right_turn()
+            counter = 0
+            #inside_Island = False
+            print("exiting island",  end=" ", flush=False)
             if distance > 25:
-                print("reseting counter to 0", end=" ", flush=True)
-                counter = 0
-        if distance < 7: #is too close to the wall
-            print("Moving away form wall", end=" ", flush=True)
-            get_further_from_wall()
-        if 12 > distance > 7: #is in the middle
-            control.forward_non_stop(35)
-            print("Moving forward at speed 35.", end=" ", flush=True)
+                lineFollower.read(set_color)
+                control.forward_non_stop(35)
+                print("went forward",  end=" ", flush=False)
+                if lineFollower_color == 'white':
+                    right_turn()
+            # control.
+
+        #leave_island_setup()
+        # if distance > 11: #is far from the wall
+        #     print("Moving closer to wall.", end=" ", flush=True)
+        #     get_closer_to_wall()
+        #     if distance > 25:
+        #         print("reseting counter to 0", end=" ", flush=True)
+        #         counter = 0
+        # if distance < 7: #is too close to the wall
+        #     print("Moving away form wall", end=" ", flush=True)
+        #     get_further_from_wall()
+        # if 12 > distance > 7: #is in the middle
+        #     control.forward_non_stop(35)
+        #     print("Moving forward at speed 35.", end=" ", flush=True)
 
         # Small delay
-        sleep(0.1)
+       # sleep(0.1)
 
 def get_further_from_wall():
     control.controlled_turn(18, 35)
@@ -97,65 +121,11 @@ def get_further_from_wall():
 def get_closer_to_wall():
     control.controlled_turn(40, 18)
 
-def leave_island_setup():
-
-    global lineFollower_color
-    global ultrasonicSensor
-    global control
-    global counter
-    global inside_Island
-
-
-    print("Leave Island Setup 180-degree turn v2", end=" ", flush=True)
-    control.move_backward(15, 1000)
-    turn_180_degrees_v2ii()
-    control.stop()
-    counter = 0
-    while True:
-        while lineFollower_color != 'white':
-            control.forward_non_stop(35)
-            sleep(0.1)
-        counter += 1
-        if counter == 1:
-            lineFollower.read(set_color)
-            # Read ultrasonic distance
-            distance = ultrasonicSensor.get_distance(port=7)
-            # If the sensor reads 400, it's typically 'no object' or out of range.
-            # Instead of forcing it to zero, we can keep it at 400 or set a default:
-
-            print("\r" + " " * 50, end=" ")  # Clears the line
-            # Add color to the distance and line color, and different colors to their values
-            print(f"\r\033[94mDistance:\033[0m \033[92m{distance}\033[0m "
-                  f" | \033[94mLine color:\033[0m \033[92m{lineFollower_color}\033[0m "
-                  f"| \033[94mCounter :\033[0m \033[92m{counter}\033[0m "
-                  f" | \033[94mInside Island:\033[0m \033[92m{inside_Island}\033[0m ", end=" ", flush=True)
-
-            if lineFollower_color == 'white':
-                print(" Wall encountered, Making right turn", end="", flush=True)
-                # check if we are in island for a 180-degree turn
-                avoid_wall()
-            if distance > 11:  # is far from the wall
-                print("Moving closer to wall.", end=" ", flush=True)
-                get_closer_to_wall()
-                if distance > 25:
-                    print("reseting counter to 0", end=" ", flush=True)
-                    counter = 0
-            if distance < 7:  # is too close to the wall
-                print("Moving away form wall", end=" ", flush=True)
-                get_further_from_wall()
-            if 12 > distance > 7:  # is in the middle
-                control.forward_non_stop(35)
-                print("Moving forward at speed 35.", end=" ", flush=True)
-
-            # Small delay
-            sleep(0.1)
-
-
 def turn_180_degrees():
     control.stop()
     # make a 180-degree turn
     control.controlled_turn(0, 28)
-    sleep(3.3)
+    sleep(2.7)
 
     # sleep(5.3)
     control.stop()
@@ -165,17 +135,24 @@ def turn_180_degrees_v2ii():
     # make a 180-degree turn
     control.move_backward(25,1000)
     print("180 v2 turn", end=" ", flush=True)
-    control.controlled_turn(-15,9)
-    sleep(6.6)
-
+    control.controlled_turn(-15,7)
+    sleep(7.6)
     control.stop()
+
+def right_turn():
+    control.stop()  # stop when front wall is hit or too close
+    control.move_backward(50, 300)  # Move back a bit
+    # Turn left 90 degrees, might need some tweaking
+    control.move_left(60, 700)
+    control.stop()  # stop moving
+    sleep(1)  # sleep for 1 second for line follower to update
 
 def avoid_wall():
     global counter
     control.stop() # stop when front wall is hit or too close
     control.move_backward(50, 300)  # Move back a bit
     # Turn left 90 degrees, might need some tweaking
-    control.move_left(50, 700)
+    control.move_left(60, 700)
     control.stop() #stop moving
     sleep(1) #sleep for 1 second for line follower to update
 
@@ -183,18 +160,12 @@ def avoid_wall():
     # Update print statement
     print(f"\r\033[94mDistance:\033[0m \033[92m{ultrasonicSensor.get_distance(port=7)}\033[0m | \033[94mLine color:\033[0m \033[92m{lineFollower_color}\033[0m", end="", flush=True)
 
-def main():
+def right_hand_rule():
+    global inside_Island
     global lineFollower_color
     global ultrasonicSensor
     global control
     global counter
-    global inside_Island
-
-    # Initial stop
-    control.stop()
-    sleep(1)
-    print("\033[92m Starting main loop...\033[0m")  # Print in green
-
     while True:
         # Continuously read the line follower data and update color
         lineFollower.read(set_color)
@@ -203,7 +174,7 @@ def main():
         # If the sensor reads 400, it's typically 'no object' or out of range.
         # Instead of forcing it to zero, we can keep it at 400 or set a default:
 
-        print("\r" + " " * 50, end=" ")  # Clears the line
+
         # Add color to the distance and line color, and different colors to their values
         print(f"\r\033[94mDistance:\033[0m \033[92m{distance}\033[0m "
               f" | \033[94mLine color:\033[0m \033[92m{lineFollower_color}\033[0m "
@@ -211,32 +182,57 @@ def main():
               f" | \033[94mInside Island:\033[0m \033[92m{inside_Island}\033[0m ", end=" ", flush=True)
 
         if lineFollower_color == 'white':
-            print(" Wall encountered, Making right turn", end="", flush=True)
             #check if we are in island for a 180-degree turn
-            avoid_wall()
-            if counter == 4:
+            if inside_Island:
+               print(" Making stop for 5 secs", end="", flush=True)
+               control.stop()
+               sleep(5)
+               right_turn()
+               sleep(1)
+               right_turn()
+               sleep(1)
+            elif counter == 4:
                 print("Inside Loop, adjusting to enter island", end="", flush=True)
                 print("making a 180-degree turn.", end="", flush=True)
                 turn_180_degrees()
                 inside_Island = True
+                # exit_island()
                 # head to island
                 head_to_island()
-        if distance > 11: #is far from the wall
+            else:
+                print(" Wall encountered, Making right turn", end="", flush=True)
+                avoid_wall()
+        if distance > 10 : #is far from the wall
             print("Moving closer to wall.", end=" ", flush=True)
             get_closer_to_wall()
             if distance > 25:
                 print("reseting counter to 0", end=" ", flush=True)
                 counter = 0
-        if distance < 7: #is too close to the wall
+            if inside_Island:
+                while lineFollower_color != 'white':
+                    control.forward_non_stop(35)
+                inside_Island = False
+        if distance < 6: #is too close to the wall
             print("Moving away form wall", end=" ", flush=True)
             get_further_from_wall()
-        if 12 > distance > 7: #is in the middle
+        if 11 > distance > 7: #is in the middle
             control.forward_non_stop(35)
             print("Moving forward at speed 35.", end=" ", flush=True)
-
         # Small delay
         sleep(0.1)
 
+
+def main():
+    global lineFollower_color
+    global ultrasonicSensor
+    global control
+    global counter
+
+    # Initial stop
+    control.stop()
+    sleep(1)
+    print("\033[92m Starting main loop...\033[0m")  # Print in green
+    right_hand_rule()
 
 # Entry point
 if __name__ == "__main__":
